5cc47bc154439a3b9247afd897fb7d0ea2465e1e
Thi So Ny Nguyen <tngu1033@my.centennialcollege.ca>
2025-04-01 21:40:40 -0400

    Add console app to consume StringExtensions NuGet package

---
+++ StringExtensionsConsumer/StringExtensionsConsumer/Program.cs
@@ -0,0 +1,13 @@
+﻿using StringExtensions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string test1 = "Hello";
+        string test2 = "world";
+
+        Console.WriteLine($"Does '{test1}' start with an uppercase letter? {test1.StartsWithUpperCase()}");
+        Console.WriteLine($"Does '{test2}' start with an uppercase letter? {test2.StartsWithUpperCase()}");
+    }
+}
